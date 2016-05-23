9b1670b8ab766e53334c1ecd7eaf1741cf151c0c
Yamin Noor <ymnoor21@gmail.com>
2016-05-22 23:50:00 -0700

    Setup virtual host

--- src/myproj/wsgi.py
+++ src/myproj/wsgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+EXTRA_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), '..', '..')) + '/src'
+if EXTRA_DIR not in sys.path:
+    sys.path.append(EXTRA_DIR)
 
 from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproj.settings")
 
 application = get_wsgi_application()
